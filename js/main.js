49e6fcf3abc4f8096e3379331710c1136c0d0d08
rogegar <rogegarhdz@gmail.com>
2022-12-12 13:47:17 -0600

    Se soluciona el primer ejercicio con strings

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let nombre="valeria duque";
+nombre=nombre.toLocaleUpperCase();
+nombre.charAt(0);
+let aux=0,aux2=0,aux3="",i=0;
+function iguales() {
+    for (let index = 0; index < nombre.length; index++) {        
+        aux=nombre.charAt(index);
+        for (let index1 = index+1; index1 < nombre.length; index1++) {
+            aux2=nombre.charAt(index1);
+            if (aux===aux2) {
+                i++;
+                aux3=aux2;
+            }
+        }   
+    }
+    if (i==0) {
+        console.log("no se repiten letras");
+    }else
+    console.log("la  letra que mas se repite es:"+ aux3 + ", se repitio: " + i +" veces");
+}
